01f3350c9ea1dcad475943596f8803012dc242c2
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2023-01-03 19:14:32 +0530

    details

---
+++ components/Details.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native';
+
+const Details = () => {
+  return (
+    <View style={styles.container}>
+        <Text>Details Page</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    
+})
+
+export default Details
