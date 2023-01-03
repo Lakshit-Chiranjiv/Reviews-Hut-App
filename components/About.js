19463cfd4f607dfd7b13dec3f36160c10c5e10b6
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2023-01-03 19:15:55 +0530

    about

---
+++ components/About.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native';
+
+const About = () => {
+  return(
+    <View style={styles.container}>
+        <Text>About Page</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    
+})
+
+export default About
