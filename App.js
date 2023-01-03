9686ba221e3e1c5c72f6b4b5e829b39eed87a224
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2023-01-03 20:06:46 +0530

    switched from app loading to splash screen

--- App.js
+++ App.js
@@ -1,36 +1,54 @@
 import { StyleSheet, Text, View } from 'react-native';
 import Home from './components/Home';
 import * as Font from 'expo-font';
-import AppLoading from 'expo-app-loading';
-import { useState } from 'react';
+import * as SplashScreen from 'expo-splash-screen';
+import { useState,useCallback, useEffect } from 'react';
 
-const getFonts = () => Font.loadAsync({
-  'prompt-regular': require('./assets/fonts/prompt/Prompt-Regular.ttf'),
-  'prompt-bold': require('./assets/fonts/prompt/Prompt-Bold.ttf'),
-  'prompt-semibold': require('./assets/fonts/prompt/Prompt-SemiBold.ttf'),
-})
 
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
 
-  const [fontsLoaded, setFontsLoaded] = useState(false);
+  const [appIsReady, setAppIsReady] = useState(false);
 
-  if (fontsLoaded) {
-    return (
-      <View style={styles.container}>
-        <Home />
-      </View>
-    );
+  useEffect(() => {
+    async function prepare() {
+      try {
+
+        await Font.loadAsync({
+          'prompt-regular': require('./assets/fonts/prompt/Prompt-Regular.ttf'),
+          'prompt-bold': require('./assets/fonts/prompt/Prompt-Bold.ttf'),
+          'prompt-semibold': require('./assets/fonts/prompt/Prompt-SemiBold.ttf'),
+        });
+
+        await new Promise(resolve => setTimeout(resolve, 2000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
   }
-  else {
-    return (
-      <AppLoading
-        startAsync={getFonts}
-        onFinish={() => setFontsLoaded(true)}
-        onError={() => console.log('error')}
-      />
-    )
-  }
+
+  return (
+    <View 
+      style={styles.container}
+      onLayout={onLayoutRootView}>
+      <Home/>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
