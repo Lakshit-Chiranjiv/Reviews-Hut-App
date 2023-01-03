710519a2d8363a35dcf836f7fad88dc9c40f905d
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2023-01-03 19:20:48 +0530

    getFonts func and fontsloaded state

--- App.js
+++ App.js
@@ -1,7 +1,20 @@
 import { StyleSheet, Text, View } from 'react-native';
 import Home from './components/Home';
+import * as Font from 'expo-font';
+import AppLoading from 'expo-app-loading';
+import { useState } from 'react';
+
+const getFonts = () => Font.loadAsync({
+  'prompt-regular': require('./assets/fonts/Prompt-Regular.ttf'),
+  'prompt-bold': require('./assets/fonts/Prompt-Bold.ttf'),
+  'prompt-semibold': require('./assets/fonts/Prompt-SemiBold.ttf'),
+})
+
 
 export default function App() {
+
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
   return (
     <View style={styles.container}>
       <Home/>
