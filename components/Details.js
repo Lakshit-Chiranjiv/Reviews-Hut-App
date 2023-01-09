73325c49e642c549d15e65f9dc84462a69ad880d
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2023-01-09 23:36:24 +0530

    imported global  styles

--- components/Details.js
+++ components/Details.js
@@ -1,18 +1,17 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native';
+import { globalStyles } from '../styles/global';
 
 const Details = () => {
   return (
-    <View style={styles.container}>
+    <View style={globalStyles.container}>
         <Text>Details Page</Text>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-    container: {
-        padding: 24
-    }
+
 })
 
 export default Details
