05606b7d5dc2ffe7ea6cf1e4a8bc2743784c17f7
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2023-01-09 23:50:38 +0530

    imports

---
+++ routes/homeStack.js
@@ -0,0 +1,7 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
+import React from "react";
+
+import Home from "./components/Home";
+import Details from "../components/Details";
+
