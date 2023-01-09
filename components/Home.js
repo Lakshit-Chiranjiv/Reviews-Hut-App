54930ad755cc04f41389867d4e3129bfc9e98690
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2023-01-09 23:29:37 +0530

    imported global  styles

--- components/Home.js
+++ components/Home.js
@@ -1,21 +1,17 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native';
+import { globalStyles } from '../styles/global';
 
 const Home = () => {
   return (
-    <View style={styles.container}>
-        <Text style={styles.text}>Home Page hh</Text>
+    <View style={globalStyles.container}>
+        <Text style={globalStyles.text}>Home Page hh</Text>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-    container: {
-        padding: 24
-    },
-    text: {
-        fontFamily: 'prompt-regular',
-    }
+
 })
 
 export default Home
