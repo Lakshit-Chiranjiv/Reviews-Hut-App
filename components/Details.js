13080af2dd16defe206fba27de4962121acfacd8
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2023-01-10 19:25:27 +0530

    rendered body text

--- components/Details.js
+++ components/Details.js
@@ -4,11 +4,12 @@ import { globalStyles } from '../styles/global';
 
 const Details = ({ route,navigation }) => {
 
-  const { title, rating } = route.params;
+  const { title, rating, body } = route.params;
 
   return (
     <View style={globalStyles.container}>
         <Text style={styles.titleText}>{title}</Text>
+        <Text style={styles.bodyText}>{body}</Text>
         <Text style={styles.ratingText}>{rating}🔥</Text>
     </View>
   )
@@ -19,8 +20,13 @@ const styles = StyleSheet.create({
     fontFamily: 'prompt-bold',
     fontSize: 24,
   },
+  bodyText: {
+    fontFamily: 'prompt-regular',
+  },
   ratingText: {
     fontFamily: 'prompt-semibold',
+    fontSize: 16,
+    marginVertical: 10,
   }
 })
 
