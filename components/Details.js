51cd263c470ebe78ca7ef87e8c8b0dc4a2200e0c
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2023-01-10 19:21:30 +0530

    received route data on details page

--- components/Details.js
+++ components/Details.js
@@ -2,16 +2,26 @@ import React from 'react'
 import { Button, StyleSheet, Text, View } from 'react-native';
 import { globalStyles } from '../styles/global';
 
-const Details = ({ navigation }) => {
+const Details = ({ route,navigation }) => {
+
+  const { title, rating } = route.params;
+
   return (
     <View style={globalStyles.container}>
-        <Text>Details Page</Text>
+        <Text style={styles.titleText}>{title}</Text>
+        <Text style={styles.ratingText}>{rating}🔥</Text>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-
+  titleText: {
+    fontFamily: 'prompt-bold',
+    fontSize: 24,
+  },
+  ratingText: {
+    fontFamily: 'prompt-semibold',
+  }
 })
 
 export default Details
