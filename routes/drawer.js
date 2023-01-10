0eaaa6f1d79bffe9a940197248851a852cf0740b
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2023-01-10 20:33:23 +0530

    imports

---
+++ routes/drawer.js
@@ -0,0 +1,7 @@
+import { createDrawerNavigator } from 'react-navigation-drawer';
+import { NavigationContainer } from '@react-navigation/native';
+import React from 'react';
+
+import AboutStackScreen from './routes/aboutStack';
+import HomeStackScreen from './routes/homeStack';
+
