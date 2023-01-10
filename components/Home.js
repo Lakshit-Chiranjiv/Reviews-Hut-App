b675b0bc44e4b21ed352fcb430dc2c28715e103b
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2023-01-10 19:10:57 +0530

    dummy data states

--- components/Home.js
+++ components/Home.js
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Button, StyleSheet, Text, View } from 'react-native';
 import { globalStyles } from '../styles/global';
 
 const Home = ({ navigation }) => {
+
+  const [reviews, setReviews] = useState([
+    { title: 'Zelda, Breath of Fresh Air', rating: 5, body: 'lorem ipsum', key: '1' },
+    { title: 'Gotta Catch Them All (again)', rating: 4, body: 'lorem ipsum', key: '2' },
+    { title: 'Not So "Final" Fantasy', rating: 3, body: 'lorem ipsum', key: '3' },
+  ])
+
   return (
     <View style={globalStyles.container}>
         <Text style={globalStyles.text}>Home Page hh</Text>
