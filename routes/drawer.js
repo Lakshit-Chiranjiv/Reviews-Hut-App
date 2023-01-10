f935ce5e6ffec150d498760b9602203222f19088
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2023-01-10 20:35:32 +0530

    Drawer component

--- routes/drawer.js
+++ routes/drawer.js
@@ -5,3 +5,16 @@ import React from 'react';
 import AboutStackScreen from './routes/aboutStack';
 import HomeStackScreen from './routes/homeStack';
 
+const RootDrawerNavigator = createDrawerNavigator()
+
+const Drawer = () => {
+
+    return (
+        <NavigationContainer>
+            <RootDrawerNavigator.Navigator>
+                <RootDrawerNavigator.Screen name="Home" component={HomeStackScreen} />
+                <RootDrawerNavigator.Screen name="About" component={AboutStackScreen} />
+            </RootDrawerNavigator.Navigator>
+        </NavigationContainer>
+    )
+}
