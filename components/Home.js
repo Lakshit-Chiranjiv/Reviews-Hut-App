5c54b68da4f6fe3e5496436740beacbb1c42753a
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2023-01-28 22:32:56 +0530

    added a review

--- components/Home.js
+++ components/Home.js
@@ -8,6 +8,7 @@ const Home = ({ navigation }) => {
     { title: 'Zelda, Breath of Fresh Air', rating: 5, body: 'lorem ipsum', key: '1' },
     { title: 'Gotta Catch Them All (again)', rating: 4, body: 'lorem ipsum', key: '2' },
     { title: 'Not So "Final" Fantasy', rating: 3, body: 'lorem ipsum', key: '3' },
+    { title: 'The Legend of Zelda: Ocarina of Tie', rating: 5, body: 'lorem ipsum', key: '4' },
   ])
 
   return (
